962c960852614f9f0c785293fbdd8c5e2d784344
wasim <wasimtestengineer007@gmail.com>
2023-03-26 15:22:14 +0530

    move Negative

---
+++ array/5_moveNegativeElement.py
@@ -0,0 +1,27 @@
+"Move all the negative elements to one side of the array "
+
+def rearrangement(arr):
+    for j in range(len(arr)):
+        for i in range(len(arr)):
+            if (arr[i]>0):
+                temp=arr[i]
+                arr[i]=arr[j]
+                arr[j]=temp
+
+    return arr
+arr=[-1,2,3,4,-9]
+print(rearrangement(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
